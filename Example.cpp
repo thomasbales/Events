e45e8621a963138ca9b56958a67cb13bdde84845
thomasbales <109531083+thomasbales@users.noreply.github.com>
2022-07-28 17:42:40 -0500

    Update comments

--- Example.cpp
+++ Example.cpp
@@ -12,13 +12,13 @@ void Func2WithParams(int param1, string param2);
 int main()
 {
     //Instantiate an event object. Specify the parameter type(s) of the 
-    //callback funtion signatures the event will be calling in angle brackets.
+    //callback function signatures the event will be calling in angle brackets.
     //Events are only compatible with callbacks with a return type of void.
 
     Event<> event;
     Event<int, string> eventWithParams;
 
-    //Add callbacks with either the Event.Add function or the += operator.
+    //Add callbacks with either the Event.AddCallback function or the += operator.
 
     event.AddCallback(Func1);
     event += Func2;
@@ -38,7 +38,7 @@ int main()
 
     cout << endl;
 
-    //Remove callbacks with either the Event.Remove function or the -= operator.
+    //Remove callbacks with either the Event.RemoveCallback function or the -= operator.
     //Be sure to remove callbacks that no longer exist (ie. functions that belong 
     //to deleted object) before calling Event.Invoke, otherwise
     //calling Event.Invoke might have undefined behaviour.
